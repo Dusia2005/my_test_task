93f234829d0d30382382890eb8df7b6107ffafab
Dasha Leshok <116278334+Dusia2005@users.noreply.github.com>
2026-02-20 11:03:08 +0300

    Initial commit

---
+++ src/test/java/com/example/my_test_task/MyTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
